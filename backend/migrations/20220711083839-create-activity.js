384442d89862c2e0353a21f826b85d74bcfe6017
Kristell Lansonneur <klansonneur@hotmail.com>
2022-09-08 12:24:21 +0200

    Update 20220711083839-create-activity.js

--- backend/migrations/20220711083839-create-activity.js
+++ backend/migrations/20220711083839-create-activity.js
@@ -6,7 +6,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INT
+        type: Sequelize.INTEGER
       },
       imageUrl: {
         type: Sequelize.STRING,
