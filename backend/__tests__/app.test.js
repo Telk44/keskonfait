846f201b7668b65838862ef03e4c38eaa6529b45
Klansonneur <klansonneur@hotmail.com>
2022-09-08 11:16:14 +0200

    eslint modif

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -9,6 +9,6 @@ describe('test get age', ()=>{
     });
     it('get the age endpoint', async () => {
         const response = await request.get('/age/')
-        expect(response.status).toBe(400)
+        expect(response.status).not.toBe(400)
     });
 })
