79df6fb2a39a1f2495b71848e50ccb7e40e16286
Kristell Lansonneur <klansonneur@hotmail.com>
2022-09-08 12:24:52 +0200

    Update 20220711091156-create-age.js

--- backend/migrations/20220711091156-create-age.js
+++ backend/migrations/20220711091156-create-age.js
@@ -1,28 +1,28 @@
-// 'use strict';
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await queryInterface.createTable('Ages', {
-//       id: {
-//         allowNull: false,
-//         autoIncrement: true,
-//         primaryKey: true,
-//         type: Sequelize.INTEGER
-//       },
-//       childrenAge: {
-//         allowNull: false,
-//         type: Sequelize.STRING
-//       },
-//       createdAt: {
-//         allowNull: false,
-//         type: Sequelize.DATE
-//       },
-//       updatedAt: {
-//         allowNull: false,
-//         type: Sequelize.DATE
-//       }
-//     });
-//   },
-//   async down(queryInterface, Sequelize) {
-//     await queryInterface.dropTable('Ages');
-//   }
-// };
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Ages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      childrenAge: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Ages');
+  }
+};
